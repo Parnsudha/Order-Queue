0084b404b5006056faa3891469ced1b39ba6ce6e
Parnsudha <133491020+Parnsudha@users.noreply.github.com>
2025-08-02 01:35:19 +0530

    Update Customer_form.py

--- Customer_form.py
+++ Customer_form.py
@@ -42,12 +42,12 @@ with st.form("customer_order_form"):
     delivery_time = st.time_input("Preferred Delivery Time", value=time(16, 0))
     address = st.text_area("Delivery Address (optional)")
 
-    st.markdown("### 📍 Click on the Map for Delivery Location")
+    with st.expander("### 📍 Click on the Map for Delivery Location", expanded=True)
 
     # Responsive, smaller height, less gap below map
-    m = folium.Map(location=[26.9124, 75.7873], zoom_start=12)
-    m.add_child(folium.LatLngPopup())
-    map_data = st_folium(m, height=340, use_container_width=True)
+        m = folium.Map(location=[26.9124, 75.7873], zoom_start=12, Height=340)
+        m.add_child(folium.LatLngPopup())
+        map_data = st_folium(m, height=340, use_container_width=True)
 
     # Button close to map, minimal spacing
     submitted = st.form_submit_button("🚀 Submit Order")
